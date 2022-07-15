4406389b4dbf7a0a25512e597822c54a202d8bda
Benedikt Rips <benedikt.rips@gmail.com>
2022-07-15 12:40:40 +0200

    Initial commit

---
+++ lua/wrap-motions.lua
@@ -0,0 +1,58 @@
+local M = {}
+
+local forBuf = function(opts)
+  if opts then
+    opts.buffer = true
+  else
+    opts = {buffer = true}
+  end
+  return opts
+end
+
+local setBufMap = function(mode, lhs, rhs, opts)
+  vim.keymap.set(mode, lhs, rhs, forBuf(opts))
+end
+
+local delBufMap = function(mode, lhs, opts)
+  vim.keymap.del(mode, lhs, forBuf(opts))
+end
+
+M.enable = function()
+  setBufMap("",  "j",      "gj")
+  setBufMap("",  "k",      "gk")
+  setBufMap("",  "<Down>", "j",           {remap = true})
+  setBufMap("",  "<Up>",   "k",           {remap = true})
+  setBufMap("",  "0",      "g0")
+  setBufMap("",  "^",      "g^")
+  setBufMap("",  "$",      "g$")
+  setBufMap("n", "A",      "$a",          {remap = true})
+  setBufMap("n", "C",      "c$",          {remap = true})
+  setBufMap("n", "cc",     "0C",          {remap = true})
+  setBufMap("n", "D",      "d$",          {remap = true})
+  setBufMap("n", "dd",     "0D",          {remap = true})
+  setBufMap("n", "Y",      "y$",          {remap = true})
+  setBufMap("n", "yy",     "0Y",          {remap = true})
+  setBufMap("i", "<Down>", "<C-o><Down>", {remap = true})
+  setBufMap("i", "<Up>",   "<C-o><Up>",   {remap = true})
+end
+
+M.disable = function()
+  delBufMap("",  "j")
+  delBufMap("",  "k")
+  delBufMap("",  "<Down>")
+  delBufMap("",  "<Up>")
+  delBufMap("",  "0")
+  delBufMap("",  "^")
+  delBufMap("",  "$")
+  delBufMap("n", "A")
+  delBufMap("n", "C")
+  delBufMap("n", "cc")
+  delBufMap("n", "D")
+  delBufMap("n", "dd")
+  delBufMap("n", "Y")
+  delBufMap("n", "yy")
+  delBufMap("i", "<Down>")
+  delBufMap("i", "<Up>")
+end
+
+return M
---
+++ plugin/wrap-motions.lua
@@ -0,0 +1,21 @@
+local this = require("wrap-motions")
+
+local augroup = vim.api.nvim_create_augroup("wrap-motions", {})
+vim.api.nvim_create_autocmd("OptionSet", {
+  group = augroup,
+  pattern = "wrap",
+  desc = "Toggle display motions",
+  callback = function()
+    -- Only proceed if `wrap` changed.
+    if vim.v.option_new == vim.v.option_old then
+      return
+    end
+
+    -- Enable/disable display motions
+    if vim.opt_local.wrap:get() then
+      this.enable()
+    else
+      this.disable()
+    end
+  end
+})
